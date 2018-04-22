97d846dff42bb23fcdd94c6f52102e8c3671adec
Steven Dorsher <sdorsher@gmail.com>
2018-04-22 15:18:57 -0500

    totals vowels, consonnents, symbols, and others per country. want histograms within a country to compare distributions

--- letterbarplot.py
+++ letterbarplot.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python
+
+#not feasible due to lack of linguistic knowledge. 
+
+
+
+
+import os, sys
+
+
+
 import numpy as np
 from io import StringIO
 import re
@@ -7,80 +18,79 @@ file = open("airports.dat.txt","r")
 
 def AirportNameLetterFrequency(name):
     
-    #has to be typable on my keyboard. I'm going to have to figure this out. For prototyping, use 26 character alphabet with - and / and space make it case insensitive. I think this is the wrong answer and am going to contemplate this overnight.
+    words = sum(c.isalpha() for c in name)
+    spaces = sum(c.isspace() for c in name)
+    others = len(name) - words - spaces
+    vowels = sum(map(name.lower().count, "aeiou"))
+    consonents = words - vowels
+    #put a one at the end to account for one airport in number totaled. will be normalized later
 
-#look at extended data tomorrow and see what can be done within a given country. starting with USA, maybe compare to canada and another country with a similar character set.
-
-
-#for now, consider only the vowels
-#M-x set-input-method latin-postfix
-    atot=name.count('a')+name.count('á')+name.count('à')+name.count('â')+name.count('ä')+name.count('ą')+name.count('ã')+name.count('å')+name.count('ª') 
-
-    etot=name.count('e')+name.count('é')+name.count('è')+name.count('ê')+name.count('ë')+name.count('ě')+name.count('ę')+name.count('ė') 
-
-    itot= name.count('i')+name.count('í')+name.count('ì')+name.count('î')+name.count('í')+name.count('ĩ')+name.count('į')+name.count('ı')  
-
-    otot=name.count('o')+name.count('ó')+name.count('ò')+name.count('ô')+name.count('ö')+name.count('õ')+name.count('ő')+name.count('ð')+name.count('º')+name.count('ø')  
-
-    utot=name.count('u')+name.count('ú')+name.count('ù')+name.count('û')+name.count('ü')+name.count('ũ')+name.count('ų')+name.count('ű')+name.count('ů') 
-
-    ytot=name.count('y')+name.count('ß')+name.count('w')+name.count('æ')+name.count('þ') 
-    Vowels = [atot etot itot otot utot ytot]
-
-    return Vowels
-
+    return [consonents, vowels, spaces, others, 1]
 
 def MergeCount(totalCount, thisCount):
-    totalCount=totalCount+thisCount
-    return totalCount
+    totalCount2=totalCount+thisCount
+    return totalCount2
 
-def CounttoHistograms(thisUSAcount, totalUSAcount, totalEnglandcount, totalTurkeycount, totalFrancecount, totalGermanycount, totalMexicocount, totalCanadacount)
-
-
+#def CounttoHistograms(thisUSAcount, totalUSAcount, totalEnglandcount, totalTurkeycount, totalFrancecount, totalGermanycount, totalMexicocount, totalCanadacount)
 
 
-totalUSAcount=Null
-totalChinacount=Null 
+
+
+totalUSAcount=np.zeros(5)
+totalChinacount=np.zeros(5)
+totalEnglandcount=np.zeros(5)
+totalTurkeycount=np.zeros(5)
+totalFrancecount=np.zeros(5)
+totalGermanycount=np.zeros(5)
+totalMexicocount=np.zeros(5)
+totalCanadacount=np.zeros(5)
+
+
+
+#I picked some countries I have familiarity with so I know whether or not the data makes sense when I see it, sort of.
 
 for line in file:
-    re.split(',',line);
-    print line
-    if line(2) == "United States":
-        thisUSAcount = AirportNameLetterFrequency(line(1))
-        totalUSAcount= MergeCount(totalUSAcount,thisUSAcount)
-    if line(2) == "China":
-        thisChinacountt = AirportNameLetterFrequency(line(1))
+    line=re.split(',',line);
+    if line[3] == "\"China\"":
+        thisChinacount = AirportNameLetterFrequency(line[1])
         totalChinacount=MergeCount(totalChinacount,thisChinacount)
-    if line(2) == "United Kingdom":
-        thisEnglandcountt = AirportNameLetterFrequency(line(1))
+    if line[3] == "\"United States\"":
+        thisUSAcount = AirportNameLetterFrequency(line[1])
+        totalUSAcount= MergeCount(totalUSAcount,thisUSAcount)
+    if line[3] == "\"United Kingdom\"":
+        thisEnglandcount = AirportNameLetterFrequency(line[1])
         totalEnglandcount=MergeCount(totalEnglandcount,thisEnglandcount)
-    if line(2) == "Turkey":
-        thisTurkeycount = AirportNameLetterFrequency(line(1))
+    if line[3] == "\"Turkey\"":
+        thisTurkeycount = AirportNameLetterFrequency(line[1])
         totalTurkeycount=MergeCount(totalTurkeycount,thisTurkeycount)
- if line(2) == "France":
-        thisFrancecountt = AirportNameLetterFrequency(line(1))
+    if line[3] == "\"France\"":
+        thisFrancecount = AirportNameLetterFrequency(line[1])
         totalFrancecount=MergeCount(totalFrancecount,thisFrancecount)
-if line(2) == "Germany":
-        thisGermanycountt = AirportNameLetterFrequency(line(1))
+    if line[3] == "\"Germany\"":
+        thisGermanycount = AirportNameLetterFrequency(line[1])
         totalGermanycount=MergeCount(totalGermanycount,thisGermanycount)
-if line(2) == "Mexico" :
-        thisMexicocountt = AirportNameLetterFrequency(line(1))
+    if line[3] == "\"Mexico\"" :
+        thisMexicocount = AirportNameLetterFrequency(line[1])
         totalMexicocount=MergeCount(totalMexicocount,thisMexicocount)
-if line(2) == "Canada" :
-        thisCanadacountt = AirportNameLetterFrequency(line(1))
+    if line[3] == "\"Canada\"" :
+        thisCanadacount = AirportNameLetterFrequency(line[1])
         totalCanadacount=MergeCount(totalCanadacount,thisCanadacount)
                 
 #ahist, ehist, ihist, ohist, uhist, yhist==CounttoHistograms(thisUSAcount, totalUSAcount, totalEnglandcount, totalTurkeycount, totalFrancecount, totalGermanycount, totalMexicocount, totalCanadacount)
 
 
-print totalUSAcount
-print totalChinacount
-print totalTurkeycount
-print totalEnglandcount
-print totalFrancecount
-print totalGermanycount
-print totalMexicocount
-print totalCanadacount
+print totalUSAcount/totalUSAcount[4]
+print totalChinacount/totalChinacount[4]
+print totalTurkeycount/totalTurkeycount[4]
+print totalEnglandcount/totalEnglandcount[4]
+print totalFrancecount/totalFrancecount[4]
+print totalGermanycount/totalGermanycount[4]
+print totalMexicocount/totalMexicocount[4]
+print totalCanadacount/totalCanadacount[4]
+
+#what I want is the histogram for china versus the histogram for mexico versus the histogram for the US.
+
+
 
 
 #fig, ax = plt.subplots()

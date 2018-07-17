561382f1a43264583c9a4efb80d6761344dd054f
Steven Dorsher <sdorsher@gmail.com>
2018-07-16 19:01:54 -0500

    The validation set shrunk relative to the training set as pointed out by Bhavika Jali. I used a colleagues phrases to compare to mine and saw that there is little spatial discrimination, perhaps because we both have lived in many places and hsare the same field, although we were discussing at least partially different subjects. I think there is also a scale problem. I attribute this to the power law tail. I suggest taking poisson initial data in the count itself rather than in the ratio to resolve this. A new algorithm will be needed.

--- classifier.py
+++ classifier.py
@@ -107,13 +107,20 @@ for i,coor in enumerate(coords):
 
 
 xtrain,xtest,ytrain,ytest=train_test_split(ratioarray,coordsarray,test_size=0.33,shuffle=True, random_state=42)
-testphrases=["Ufda!", "Howdy, pardner", "Where are we?", "I thank whatever gods may be for my unconquerable soul.", "EVERYTHING LOUDER THAN EVERYTHING ELSE!!!"]
+testphrases=["Ufda!", "I just dont know about that", "Howdy, pardner", "Where are we?", "I thank whatever gods may be for my unconquerable soul.", "I welcome our new robot overlords", "l33t h4k3rs", "I know you I walked with you once upon a dream", "These stories dont mean anything if you have no one to tell them to", "Baby you have the sort of hands that rip me apart","Multi-messenger astronomy", "Rainbow flag", "I prefer They or He?", "numerical relativity", "LIGO", "scalar field", "Osculating Orbits", "Monte-Carlo Simulation", "Data Analysis", "Data Science", "Parallelization", "Paralyzation", "Partial disability", "Non-epileptic seizures", "Wednesday Lunch", "Tuesday Lunch", "Thursday Lunch", "Guild Wars", "Elvenar", "Good Apple"]
+
+colleaguephrases=["general relativity", "black hole", "loop quantum gravity",  "quantization", "space-time", "Hamiltonian constraint", "Ashtekar", "LiSA", "LIGO", "group", "white hole", "scalar field", "numerical relativity", "cosmology", "diffeomorphism", "continuum limit", "David Berger", "Reisner-Nordstrom", "black hole spacetime", "initial data", "interpretation of quantum mechanics"]
 
 xphrases=[]
 for phrase in testphrases:
     print(phrase)
     vtc, stc, otc = VowelToConsRatio(phrase)
     xphrases.append([vtc,stc,otc])
+xcolleague=[]
+for phrase in colleaguephrases:
+    print(phrase)
+    vtc, stc, otc = VowelToConsRatio(phrase)
+    xcolleague.append([vtc,stc,otc])
 
 
 
@@ -121,7 +128,7 @@ scaler = StandardScaler()
 xtrain2=scaler.fit_transform(xtrain)
 xtest2=scaler.fit_transform(xtest)
 xphrases2=scaler.fit_transform(xphrases)
-
+xcolleague2=scaler.fit_transform(xcolleague)
 
 
 #reimplementing OneHotEncoder, in order to map numeric data to geographic output classes for ytrain
@@ -134,14 +141,17 @@ xphrases2=scaler.fit_transform(xphrases)
 #    ytestmatrix[i,int(ytest)]=1
 
 
-kpca=KernelPCA(n_components=3, random_state=42, kernel="poly", degree=3, tol=1e-3, max_iter=None, remove_zero_eig=False, fit_inverse_transform=True)
+#change to poisson input data instead of ratio input data? I think the power law tail is what is being missed and what is causing the validation set to shrink. 
+
+kpca=KernelPCA(n_components=9, random_state=42, kernel="poly", degree=3, tol=1e-3, max_iter=None, remove_zero_eig=False, fit_inverse_transform=True)
 X_kpca_train=kpca.fit_transform(xtrain2)
 X_kpca_test=kpca.fit_transform(xtest2)
 X_kpca_phrases=kpca.fit_transform(xphrases2)
+X_kpca_colleague=kpca.fit_transform(xcolleague2)
 
-print(xtrain2[0])
+print(X_kpca_phrases[0])
 print(X_kpca_train[0])
-
+print(X_kpca_colleague[0])
 
 print(np.shape(X_kpca_train))
 print(np.shape(ytrain))
@@ -150,12 +160,17 @@ lr3to2_long=LinearRegression()
 lr3to2_lat.fit(X_kpca_train,ytrain[:,0])
 lr3to2_long.fit(X_kpca_train,ytrain[:,1])
 
+
+
 latparams=lr3to2_lat.get_params()
 longparams=lr3to2_long.get_params()
 latprediction=lr3to2_lat.predict(X_kpca_train)
 longprediction=lr3to2_long.predict(X_kpca_train)
 phraselatpred=lr3to2_lat.predict(X_kpca_phrases)
 phraselongpred=lr3to2_long.predict(X_kpca_phrases)
+phraselatcolleague=lr3to2_lat.predict(X_kpca_colleague)
+phraselongcolleague=lr3to2_long.predict(X_kpca_colleague)
+
 
 print(testphrases)
 print(phraselatpred)
@@ -166,12 +181,22 @@ plt.figure()
 plt.title("Verification data for latitudes and longitudes in the US")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
-plt.scatter(longprediction,latprediction,c='r')
-plt.scatter(phraselongpred[0],phraselatpred[1],c='b',label=testphrases[0])
-plt.scatter(phraselongpred[1],phraselatpred[1],c='m',label=testphrases[1])
-plt.scatter(phraselongpred[2],phraselatpred[1],c='c',label=testphrases[2])
-plt.scatter(phraselongpred[3],phraselatpred[1],c='g',label=testphrases[3])
-plt.scatter(phraselongpred[4],phraselatpred[1],c='y',label=testphrases[4])
+plt.scatter(ytrain[:,1], ytrain[:,0], c='c', label="Training set")
+plt.scatter(longprediction,latprediction,c='r', label="Validation set")
+plt.scatter(phraselongpred,phraselatpred,c='b',label="My test phrases, I live in Minnesota right now")
+plt.scatter(phraselongcolleague, phraselatcolleague, c='y', label="Colleagues test phrases, he lives in Louisiana right now")
+#plt.scatter(phraselongpred[0],phraselatpred[1],c='b',label=testphrases[0])
+#plt.scatter(phraselongpred[1],phraselatpred[1],c='m',label=testphrases[1])
+#plt.scatter(phraselongpred[2],phraselatpred[1],c='c',label=testphrases[2])
+#plt.scatter(phraselongpred[3],phraselatpred[1],c='g',label=testphrases[3])
+#plt.scatter(phraselongpred[4],phraselatpred[1],c='y',label=testphrases[4])
+#plt.scatter(phraselongpred[5],phraselatpred[1],c='y',label=testphrases[5])
+#plt.scatter(phraselongpred[6],phraselatpred[1],c='y',label=testphrases[6])
+#plt.scatter(phraselongpred[7],phraselatpred[1],c='y',label=testphrases[7])
+#plt.scatter(phraselongpred[8],phraselatpred[1],c='y',label=testphrases[8])
+#plt.scatter(phraselongpred[9],phraselatpred[1],c='y',label=testphrases[9])
+#plt.scatter(phraselongpred[10],phraselatpred[1],c='y',label=testphrases[10])
+#plt.scatter(phraselongpred[11],phraselatpred[1],c='y',label=testphrases[11])
 plt.legend(loc="lower right")
 plt.show()
 
